278e4b789ef32dece87e749a4b09e056f55fb9ac
Victor Han <liyanghan1112@gmail.com>
2022-07-22 14:30:38 -0500

    Initial Commit

---
+++ Units Conversion/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Units Conversion
+//
+//  Created by Victor Han on 2022/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Units Conversion/Units_ConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Units_ConversionApp.swift
+//  Units Conversion
+//
+//  Created by Victor Han on 2022/7/22.
+//
+
+import SwiftUI
+
+@main
+struct Units_ConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
